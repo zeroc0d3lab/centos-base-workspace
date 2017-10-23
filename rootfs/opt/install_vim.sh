d46cd367118f2656a82cdd78860c4d7752416958
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-10-23 13:23:21 +0700

    Fixing vim installation

--- rootfs/opt/install_vim.sh
+++ rootfs/opt/install_vim.sh
@@ -7,6 +7,7 @@ DEFAULT_LUAROCKS='2.4.3'
 INSTALL_VERSION=$DEFAULT_VERSION
 INSTALL_LUA=$DEFAULT_LUA
 INSTALL_LUAROCKS=$DEFAULT_LUAROCKS
+HOME_PATH='/root'
 
 logo() {
   echo "--------------------------------------------------------------------------"
@@ -78,53 +79,53 @@ install_vim() {
   # -) vim
   # -) vundle + themes
   #-----------------------------------------------------------------------------
-  rm -rf $HOME/vim \
-      && git clone https://github.com/vim/vim.git $HOME/vim \
+  rm -rf $HOME_PATH/vim \
+      && git clone https://github.com/vim/vim.git $HOME_PATH/vim \
   #   && sudo rm -rf /usr/local/share/vim /usr/bin/vim \
-      && cd $HOME/vim \
+      && cd $HOME_PATH/vim \
       && git checkout v$INSTALL_VERSION \
       && cd src \
       && make autoconf \
       && ./configure \
-            --prefix=/usr \
-            --enable-multibyte \
-            --enable-perlinterp=dynamic \
-            --enable-rubyinterp=dynamic \
-            --with-ruby-command=`which ruby` \
-            --enable-pythoninterp=dynamic \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-            --enable-python3interp \
-            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-            --enable-luainterp \
-            --with-luajit \
-            --with-lua-prefix=/usr/include/lua5.1 \
-            --enable-cscope \
-            --enable-gui=auto \
-            --with-features=huge \
-            --with-x \
-            --enable-fontset \
-            --enable-largefile \
-            --disable-netbeans \
-            --with-compiledby="ZeroC0D3 Team" \
-            --enable-fail-if-missing \
+            # --prefix=/usr \
+            # --enable-multibyte \
+            # --enable-perlinterp=dynamic \
+            # --enable-rubyinterp=dynamic \
+            # --with-ruby-command=`which ruby` \
+            # --enable-pythoninterp=dynamic \
+            # --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            # --enable-python3interp \
+            # --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+            # --enable-luainterp \
+            # --with-luajit \
+            # --with-lua-prefix=/usr/include/lua5.1 \
+            # --enable-cscope \
+            # --enable-gui=auto \
+            # --with-features=huge \
+            # --with-x \
+            # --enable-fontset \
+            # --enable-largefile \
+            # --disable-netbeans \
+            # --with-compiledby="ZeroC0D3 Team" \
+            # --enable-fail-if-missing \
       && make distclean \
       && make \
       && cp config.mk.dist auto/config.mk \
       && sudo make install \
       && sudo mkdir -p /usr/share/vim \
       && sudo mkdir -p /usr/share/vim/vim80/ \
-      && sudo cp -fr $HOME/vim/runtime/** /usr/share/vim/vim80/
+      && sudo cp -fr $HOME_PATH/vim/runtime/** /usr/share/vim/vim80/
 
-  git clone https://github.com/zeroc0d3/vim-ide.git $HOME/vim-ide \
-      && sudo /bin/sh $HOME/vim-ide/step02.sh
+  git clone https://github.com/zeroc0d3/vim-ide.git $HOME_PATH/vim-ide \
+      && sudo /bin/sh $HOME_PATH/vim-ide/step02.sh
 
   git clone https://github.com/dracula/vim.git /opt/vim-themes/dracula \
       && git clone https://github.com/blueshirts/darcula.git /opt/vim-themes/darcula \
-      && mkdir -p $HOME/.vim/bundle/vim-colors/colors \
-      && cp /opt/vim-themes/dracula/colors/dracula.vim $HOME/.vim/bundle/vim-colors/colors/dracula.vim \
-      && cp /opt/vim-themes/darcula/colors/darcula.vim $HOME/.vim/bundle/vim-colors/colors/darcula.vim
+      && mkdir -p $HOME_PATH/.vim/bundle/vim-colors/colors \
+      && cp /opt/vim-themes/dracula/colors/dracula.vim $HOME_PATH/.vim/bundle/vim-colors/colors/dracula.vim \
+      && cp /opt/vim-themes/darcula/colors/darcula.vim $HOME_PATH/.vim/bundle/vim-colors/colors/darcula.vim
 
-  tar zcvf vim.tar.gz $HOME/vim $HOME/.vim \
+  tar zcvf vim.tar.gz $HOME_PATH/vim $HOME_PATH/.vim \
       && mv vim.tar.gz /opt
 }
 
